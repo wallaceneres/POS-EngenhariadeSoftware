dde32f5a73e545a0c7902226a61daf087b0333bc
Wallace Neres <wallace.souzza@live.com>
2024-07-28 14:47:11 -0300

    Construtores padrao e sobrecarregados

--- POO/App.java
+++ POO/App.java
@@ -6,5 +6,8 @@ public class App {
         Cao cachorro = new Cao();
         cachorro.nome = "Bobby";
         cachorro.Anda();
+
+        Cao dog = new Cao("Fox", 3);
+        dog.DadosCao();
     }
 }
--- POO/Cao.java
+++ POO/Cao.java
@@ -6,8 +6,24 @@ public class Cao {
     int idade;
     double peso;
 
+    public Cao()
+    {
+        cor ="Caramelo";
+    }
+
+    public Cao(String nome, int idade)
+    {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
     public void Anda()
     {
-        System.out.println("Estou andando...");
+        System.out.println("Estou andando..." + cor);
+    }
+
+    public void DadosCao()
+    {
+        System.out.println(nome + " " + idade);
     }
 }

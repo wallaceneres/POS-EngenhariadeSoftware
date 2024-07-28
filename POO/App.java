898728690fa0cacf1b92ccf4d39409554d2a0504
Wallace Neres <wallace.souzza@live.com>
2024-07-28 14:39:06 -0300

    Instanciando uma classe

--- POO/App.java
+++ POO/App.java
@@ -3,6 +3,8 @@ package POO;
 public class App {
     public static void App()
     {
-        
+        Cao cachorro = new Cao();
+        cachorro.nome = "Bobby";
+        cachorro.Anda();
     }
 }
---
+++ POO/Cao.java
@@ -0,0 +1,13 @@
+package POO;
+
+public class Cao {
+    String nome;
+    String cor;
+    int idade;
+    double peso;
+
+    public void Anda()
+    {
+        System.out.println("Estou andando...");
+    }
+}
